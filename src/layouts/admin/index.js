b0178aa520d9478d4305d637ce00793ed4e1ac76
Arshad vp <Arshad1071@yahoo.in>
2024-02-29 16:01:16 +0530

    ui added

--- src/layouts/admin/index.js
+++ src/layouts/admin/index.js
@@ -1,10 +1,49 @@
 import React from "react";
 import ResponsiveDrawer from "../../components/sidebar";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import BasicColor from "../../components/graph";
+import PieActiveArc from "../../components/PieChart";
+import BasicTable from "../../components/table";
+import ActionAreaCard from "../../components/card";
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 const Admin = () => {
   return (
     <div>
-      <ResponsiveDrawer />
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container>
+          <Grid item xs={2}>
+            <ResponsiveDrawer />
+          </Grid>
+          <Grid item container spacing={6} padding={5} xs={10}>
+            <Grid item xs={12}>
+              <Item>xs=12</Item>
+            </Grid>
+            <Grid item xs={8}>
+              <BasicColor />
+            </Grid>
+            <Grid item xs={4}>
+              <PieActiveArc />
+            </Grid>
+            <Grid item xs={9}>
+              <BasicTable />
+            </Grid>
+            <Grid item xs={3}>
+              <ActionAreaCard />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 };
